b1c5b9011c1cdb4325ecfb21db5daa43a506d0c3
Yauheniya Viarbitskaya <evgeniya.verbiczkaya98@gmail.com>
2018-09-28 18:13:49 +0300

    f

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    for (let index = 0; index < str.length; index++) {
+        let stack_last = stack[stack.length - 1];
+        for (let element = 0; element < bracketsConfig.length; element++) {
+            if (bracketsConfig[element][0] == bracketsConfig[element][1]) {
+                if (str[index] == bracketsConfig[element][1] && stack_last == bracketsConfig[element][0]) {
+                    stack.pop();
+                } else if (str[index] == bracketsConfig[element][0]) {
+                    stack.push(str[index]);
+                }
+            } else if (str[index] == bracketsConfig[element][0]) {
+                stack.push(str[index]);
+            } else if (str[index] == bracketsConfig[element][1] && stack_last != bracketsConfig[element][0]) {
+                stack.push(str[index]);
+            } else if (str[index] == bracketsConfig[element][1]) {
+                stack.pop();
+            }
+        }
+    }
+    if (stack.length != 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
